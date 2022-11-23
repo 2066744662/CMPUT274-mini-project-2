1799ea08ce60f38b1ab92fa96ce96c512d814111
Robbie Feng <48000455+2066744662@users.noreply.github.com>
2022-11-23 01:33:16 -0700

    Add files via upload

--- load-json.py
+++ load-json.py
@@ -1,9 +1,10 @@
+from math import ceil
 from os import system
 import time
-
+import threading
 import bson
-from pymongo import MongoClient
-global db
+import pymongo
+global db, start_time
 
 def connect():
     """
@@ -12,31 +13,35 @@ def connect():
     :param filename: file name (string)
     :return: (Collection) pointing to the dblp collection
     """
-    global db
+    global db, start_time
     port = input("Input port number: ")
     filename = input("Input filename: ")
-    client = MongoClient('mongodb://localhost:' + port)
+    start_time = time.time()
+    client = pymongo.MongoClient('mongodb://localhost:' + port)
     db = client["291db"]
     collist = db.list_collection_names()
     if "dblp" in collist:
         db["dblp"].drop()
-    if "temp" in collist:
-        db["temp"].drop()
-    system("mongoimport --port=" + port + " --db=291db --collection=temp --file=" + filename)
-    return db["temp"]
+    system("mongoimport --port=" + port + " --db=291db --collection=dblp --file=" + filename)
+    return db["dblp"]
 
 def optimize():
-    start_time = time.time()
+    """
+    generate token for each document
+    """
     collist = db.list_collection_names()
-    temp = db["temp"]
     dblp = db["dblp"]
-    results = temp.find()
-    for result in results:
-        values = list(result.values())
-        token = str(tokenize(values))
-        temp.update_one({"_id":values[0]},{"$set":{"token":token}})
-    print("--- %s seconds ---" % (time.time() - start_time))
-
+    total = dblp.count_documents({})
+    threadLock = threading.Lock()
+    threads = []
+    thread1 = myThread(1, "Thread-1",total)
+    thread2 = myThread(2, "Thread-2",total)
+    thread1.start()
+    thread2.start()
+    threads.append(thread1)
+    threads.append(thread2)
+    for t in threads:
+        t.join()
 
 def tokenize(object):
     """
@@ -44,7 +49,7 @@ def tokenize(object):
     :param object: object to tonkenize
     :return: (set) set of tokens
     """
-    if isinstance(object,list):
+    if isinstance(object, list):
         s = set()
         for o in object:
             ret = tokenize(o)
@@ -61,20 +66,27 @@ def tokenize(object):
     else:
         raise Exception(str(type(object))+" is not expected :(")
 
+class myThread (threading.Thread):
+    def __init__(self, threadID, name,total):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.total = total
+    def run(self):
+        if self.threadID == 1:
+            results = db["dblp"].find().limit(int(self.total/2))
+        else:
+            results = db["dblp"].find().sort("_id",-1).limit(self.total - int(self.total/2))
 
+        for result in results:
+            values = list(result.values())
+            token = str(tokenize(values))
+            db["dblp"].update_one({"_id": values[0]}, {"$set": {"token": token}})
+        print("--- %s seconds ---" % (time.time() - start_time))
 
-
-
-
-def develope():
-    global db
-    print('delete me')
-    client = MongoClient('mongodb://localhost:18372')
-    db = client["291db"]
 
 def main():
     connect()
-    #develope()
     optimize()
 
 
