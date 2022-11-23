a07d0594e4bb19a72323d85d6dfe35fefd0d7b6a
Robbie Feng <48000455+2066744662@users.noreply.github.com>
2022-11-22 19:29:56 -0700

    Add files via upload

--- load-json.py
+++ load-json.py
@@ -1,29 +1,81 @@
 from os import system
+from random import random
+
+import bson
 from pymongo import MongoClient
+global db
 
-
-def connect(port, filename):
+def connect():
     """
     Connect to MongoDB on given port and create 291db database.
     :param port: port number (string)
     :param filename: file name (string)
     :return: (Collection) pointing to the dblp collection
     """
-
+    global db
+    port = input("Input port number: ")
+    filename = input("Input filename: ")
     client = MongoClient('mongodb://localhost:' + port)
     db = client["291db"]
     collist = db.list_collection_names()
-    print(collist)
     if "dblp" in collist:
         db["dblp"].drop()
-    system("mongoimport --port=" + port + " --db=291db --collection=dblp --file=" + filename)
-    return db["dblp"]
+    if "temp" in collist:
+        db["temp"].drop()
+    system("mongoimport --port=" + port + " --db=291db --collection=temp --file=" + filename)
+    return db["temp"]
 
+def optimize():
+    collist = db.list_collection_names()
+    if "temp" in collist:
+        db["temp"].drop()
+    temp = db["temp"]
+    dblp = db["dblp"]
+    results = temp.find()
+    for result in results:
+        values = list(result.values())
+        id = str(tokenize(values))
+        dblp.insert_one({"_id":id,"authors":result["authors"]})
+
+
+def tokenize(object):
+    """
+    Convert input into tokens.
+    :param object: object to tonkenize
+    :return: (set) set of tokens
+    """
+    if isinstance(object,list):
+        s = set()
+        for o in object:
+            ret = tokenize(o)
+            s = s.union(ret)
+        return s
+    elif isinstance(object, str):
+        return set(object.split())
+    elif isinstance(object, int):
+        s = set()
+        s.add(str(object))
+        return s
+    elif isinstance(object, bson.objectid.ObjectId):
+        return set()
+    else:
+        raise Exception(str(type(object))+" is not expected :(")
+
+
+
+
+
+
+def develope():
+    global db
+    print('delete me')
+    client = MongoClient('mongodb://localhost:18372')
+    db = client["291db"]
 
 def main():
-    port = input("Input port number: ")
-    filename = input("Input filename: ")
-    dblp = connect(port, filename)
+    connect()
+    #develope()
+    optimize()
 
 
 if __name__ == "__main__":
